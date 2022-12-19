2daa6db5e0efa962c3e87f4b4e6b0541c34872d1
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-12-19 23:04:35 +0200

    add react-redux to App. index.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,59 +2,72 @@ import React from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
-import { nanoid } from 'nanoid';
+
 import { Section } from './App.styled';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getContacts } from 'redux/contacts/contact-selectors';
+import { getFilter } from 'redux/filter/filter-selectors';
+import { addContact, removeContact } from 'redux/contacts/contact-slice';
+import { setFilter } from 'redux/filter/filter-slice';
 
 export default function App() {
-  const [contacts, setContacts] = useState(() => {
-    const contacts = JSON.parse(localStorage.getItem('contacts'));
-    return (
-      contacts ?? [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ]
-    );
-  });
-
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+  const dispatch = useDispatch();
   useEffect(() => {
     return () => {
       localStorage.removeItem('contacts');
     };
   }, []);
 
+
+// export default function App() {
+//   const [contacts, setContacts] = useState(() => {
+//     const contacts = JSON.parse(localStorage.getItem('contacts'));
+//     return (
+//       contacts ?? [
+//         { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//         { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//         { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//         { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//       ]
+//     );
+//   });
+
+//   const [filter, setFilter] = useState('');
+
+//   useEffect(() => {
+//     localStorage.setItem('contacts', JSON.stringify(contacts));
+//   }, [contacts]);
+
+//   useEffect(() => {
+//     return () => {
+//       localStorage.removeItem('contacts');
+//     };
+//   }, []);
+
   const addContact = contact => {
     if (isDuplicate(contact)) {
       return alert(`${contact.name} is already in contacts`);
     }
-
-    setContacts(prev => {
-      const newContact = {
-        id: nanoid(),
-        ...contact,
-      };
-      return [...prev, newContact];
-    });
+    const action = addContact(contact);
+    dispatch(action);
   };
 
   const removeContact = id => {
-    setContacts(prev => {
-      const newContacts = prev.filter(item => item.id !== id);
-      return newContacts;
-    });
+  const action = removeContact(id);
+    dispatch(action);
   };
+  //   setContacts(prev => {
+  //     const newContacts = prev.filter(item => item.id !== id);
+  //     return newContacts;
+  //   });
+  // };
 
   const handleChange = e => {
     const { value } = e.currentTarget;
-    setFilter(value);
+    dispatch(setFilter(value));
   };
 
   const isDuplicate = ({ name }) => {
